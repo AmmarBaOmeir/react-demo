d79c807fb08b3a91c26745b37ffa863b2db2a828
Ammar BaOmier <bamohommad@clickapps.co>
2020-12-29 18:03:59 +0300

    nav initial settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <Link className="App-link" to="/PeopleList">
+            People List
+          </Link>
+          <Link className="App-link" to="/ToDoList">
+            To Do List
+          </Link>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/PeopleList">
+              <PeopleList />
+            </Route>
+            <Route path="/ToDoList">
+              <ToDoList />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function PeopleList() {
+  return <h2>PeopleList</h2>;
+}
+
+function ToDoList() {
+  return <h2>ToDoList</h2>;
 }
 
 export default App;
